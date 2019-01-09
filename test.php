9059871f42ac225abf8449c9e0842499500631fd
qq1204738767 <1204738767@qq.com>
2019-01-09 22:04:54 +0800

    添加

--- test.php
+++ test.php
@@ -17,3 +17,29 @@ echo '<pre>';
 print_r(getarr($arr));
 
 echo '<hr>';
+
+// 案例预编译语句
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "stu";
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+// 预处理及绑定
+$stmt = $conn->prepare("INSERT INTO yubian (firstname, lastname, email) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $firstname, $lastname, $email);
+// 设置参数并执行
+$firstname = "John";
+$lastname = 5;
+$email = "john@example.com";
+$stmt->execute();
+ 
+echo "新记录插入成功";
+ 
+$stmt->close();
+$conn->close();
